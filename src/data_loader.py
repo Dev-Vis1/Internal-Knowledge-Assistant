644d550696e2222e3bd5f0473ce433c7f482cf77
Ayodele Matthew <55578450+Dev-Vis1@users.noreply.github.com>
2026-07-27 17:13:13 +0200

    Begin Modular coding: Pipeline structuring

---
+++ src/data_loader.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+import os
+from typing import List, Dict, Any
+from langchain_community.document_loaders import (
+    DirectoryLoader,
+    PyPDFLoader,
+    PyMuPDFLoader,
+    TextLoader,
+    CSVLoader,
+    Docx2txtLoader,
+    UnstructuredHTMLLoader as HTMLLoader,
+    UnstructuredPowerPointLoader as PPTXLoader,
+    SQLDatabaseLoader,
+    JSONLoader,
+)
+
+File_type_and_loaders = {
+    "txt": TextLoader,
+    "pdf": PyMuPDFLoader,
+    "csv": CSVLoader,
+    "docx": Docx2txtLoader,
+    "html": HTMLLoader,
+    "pptx": PPTXLoader,
+    "sql": SQLDatabaseLoader,
+    "json": JSONLoader,
+    "yaml": TextLoader,
+    "xml": TextLoader
+
+}
+
+def load_all_documents_from_directory(directory_path: str) -> Dict[str, List[Any]]:
+    """
+    Loads all documents from the specified directory and returns them as a list of document objects.
+
+    Args:
+        directory_path (str): Path to the directory containing documents.
+
+    Returns:
+        Dict[str, List[Dict[str, Any]]]: A dictionary mapping file types to lists of loaded documents.
+    """
+    #loaders for each file type
+    loaded_documents = {}
+    base_directory = Path(directory_path).resolve()
+
+    for file_type, loader_cls in File_type_and_loaders.items():
+
+        #if folder type does not exist, skip to next file type
+        file_type_directory = base_directory / f"{file_type}_files"
+        print(file_type_directory)
+
+        if not file_type_directory.is_dir():
+            print(f"Directory for {file_type} files does not exist. Skipping.")
+            continue
+
+        files = list(file_type_directory.glob(f"*.{file_type}"))
+        print(f"Found {len(files)} {file_type.upper()} files in {file_type_directory}.")
+
+        for file_path in files:
+
+                filename = file_path.name
+
+                print(f"\n Processing {filename}")
+
+                try:
+                    loader = loader_cls(str(file_path))
+                    documents = loader.load()
+
+                    for doc in documents:
+                        doc.metadata["source"] = filename
+                        doc.metadata["total_pages"] = len(documents)
+                        doc.metadata["file_size"] = file_path.stat().st_size
+                        doc.metadata["file_type"] = file_type
+
+                    loaded_documents.setdefault(file_type, []).extend(documents)
+                    print(f"Successfully loaded {len(documents)} page(s) from {filename} using {loader_cls.__name__}.")
+
+                except Exception as e:
+                    print(f"Failed to load {filename} with {loader_cls.__name__}: {e}. ")
+
+    print(f"\n Total documents loaded: {sum(len(docs) for docs in loaded_documents.values())}")
+        
+    return loaded_documents
+            
+if __name__ == "__main__":
+    all_documents = load_all_documents_from_directory("./data")
---
+++ src/embedding.py
@@ -0,0 +1,53 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from typing import List, Dict, Any
+
+class EmbeddingPipeline:
+
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2", chunk_size: int = 500, chunk_overlap: int = 20):
+        self.model_name = model_name
+        self.model = SentenceTransformer(self.model_name)
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        print(f"Initialized with Embedding model: {self.model}")
+
+    def split_documents(self, documents: Any) -> List[Any]:
+        """
+        Splits documents into smaller chunks based on the specified chunk size and overlap.
+
+        Args:
+            documents (List[Any]): List of document objects to be split.
+        """
+
+        if isinstance(documents, dict):
+            documents = [doc for docs_list in documents.values() for doc in docs_list]
+
+        if not isinstance(documents, list):
+            raise TypeError("documents must be a list of Document objects or a dict of lists.")
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+            length_function=len,
+            separators=["\n\n", "\n", " ", ""]
+        )
+
+        chunks = text_splitter.split_documents(documents)
+        print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+        return chunks
+
+    def generate_embeddings(self, texts: List[str]) -> np.ndarray:
+
+        if not self.model:
+            raise ValueError("Model is not loaded. Call _load_model() first.")
+
+        print(f"Generating embeddings for {len(texts)} texts using model: {self.model_name}.")
+
+        try:
+            embeddings = self.model.encode(texts, convert_to_numpy=True, show_progress_bar=True)
+            print(f"Successfully generated embeddings. Shape: {embeddings.shape}")
+            return embeddings
+        except Exception as e:
+            print(f"Failed to generate embeddings. Error: {e}")
+            raise
